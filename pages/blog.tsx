72db446d48357b14e2ea0e930b033e1f823e4d17
L. Yasril Imam <60500658+yasrilimam98@users.noreply.github.com>
2022-06-22 06:50:17 +0700

    get static props untuk data yang bersifat static

--- pages/blog.tsx
+++ pages/blog.tsx
@@ -1,11 +1,37 @@
 import Layout from "../components/Layout";
-import Head from "next/head";
-export default function Blog() {
+import styles from "../styles/Blog.module.css";
+
+interface Post {
+  id: number;
+  title: string;
+  body: string;
+}
+
+interface BlogProps {
+  dataBlog: Post[];
+}
+export default function Blog(props: BlogProps) {
+  const { dataBlog } = props;
   return (
     <div>
       <Layout pageTitle="Blog">
-        <h1 className="title">Blog Page</h1>
+        {dataBlog.map((blog) => (
+          <div key={blog.id} className={styles.card}>
+            <h3>{blog.title}</h3>
+            <p>{blog.body}</p>
+          </div>
+        ))}
       </Layout>
     </div>
   );
+}
+
+export async function getServerSideProps() {
+  const res = await fetch("https://jsonplaceholder.typicode.com/posts");
+  const dataBlog = await res.json();
+  return {
+    props: {
+      dataBlog,
+    },
+  };
 }
--- pages/users/index.tsx
+++ pages/users/index.tsx
@@ -1,20 +1,27 @@
 import Layout from "../../components/Layout";
+import { useRouter } from "next/router";
+import styles from "../../styles/Users.module.css";
 
 interface UsersProps {
   dataUsers: Array<any>;
 }
 export default function Users(props: UsersProps) {
   const { dataUsers } = props;
+  const router = useRouter();
   console.log(dataUsers);
 
   return (
     <div>
       <Layout pageTitle="User Page">
         {dataUsers.map((user) => (
-          <>
-            <h1>{user.name}</h1>
+          <div
+            key={user.id}
+            onClick={() => router.push(`/users/${user.id}`)}
+            className={styles.card}
+          >
+            <p>{user.name}</p>
             <p>{user.email}</p>
-          </>
+          </div>
         ))}
       </Layout>
     </div>
